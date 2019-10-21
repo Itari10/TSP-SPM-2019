4c79a71fefe090f0ee6c68f24baeaa0fe15ba2d3
Justin Martin <justinma@mtu.edu>
2019-10-21 13:34:19 -0400

    Fixing CSS for Player Boxes

--- example-react-app/src/PlayerBox.js
+++ example-react-app/src/PlayerBox.js
@@ -3,10 +3,10 @@ import './PlayerBox.css';
 
 const PlayerBox = (props) => {
     return (
-        <div className={"player" + props.playerNumber}>
+        <div className="playerBox" id={props.playerNumber}>
             <h1>Player {props.playerNumber}:</h1>
         </div>
     );
-}
+};
 
 export default PlayerBox;

3964f8252cd40df19428f42d8985d9219e9af7de
Nick McCarter <nmccarter10@gmail.com>
2019-10-22 16:05:19 -0400

    Deleted test push text files.

--- example-react-app/src/Square.js
+++ example-react-app/src/Square.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Square.css';
 
+/*
+Square should be aware of it's coordinates, whether or not it's selected and whether or not it's highlighted
+*/
 const Square = (props) => {
+    const [isSelected, setSelected] = React.useState(0);
+    const [isHighlighted, setHighlighted] = React.useState(0);
+
     return (
         <button className="square"></button>
     );

0206244af946d65e142c934319ed26c313345316
Joe Kurtz <jwkurtz@mtu.edu>
2019-10-27 19:28:24 -0400

    Restructured App / Board / Square using classes to see what works

--- chess-game/src/Components/App.js
+++ chess-game/src/Components/App.js
@@ -4,25 +4,39 @@ import PlayerBox from './PlayerBox';
 import Board from './Board';
 import EndTurnBtn from './EndTurnBtn';
 
-function App() {
-    //state of players turn in game
-    const [playerOneTurn, setPlayerOneTurn] = React.useState(true);
-    //changes the players turn in the game
-    const setTurn = () => {
-        setPlayerOneTurn(!playerOneTurn);
-    };
-
-  return (
-    <div className="App">
-      <div className="Header">
-        <h1>CHESS</h1>
-      </div>
-      <EndTurnBtn onClick={setTurn}/>
-      <PlayerBox playerNumber="1" isTurn={playerOneTurn}/>
-      <PlayerBox playerNumber="2" isTurn={!(playerOneTurn)}/>
-      <Board />
-    </div>
-  );
+  class App extends React.Component{
+    
+    entireBoard = Array(8).fill(Array(8).fill(null));
+    //state;
+    
+    constructor(props){
+      super(props);
+      //this.entireBoard = Array(8).fill(Array(8).fill(null));
+      //this.state = null;
+    }
+    
+    
+    render() {
+      //state of players turn in game
+      const [playerOneTurn, setPlayerOneTurn] = React.useState(true);
+      //changes the players turn in the game
+      const setTurn = () => {
+          setPlayerOneTurn(!playerOneTurn);
+      };
+      return (
+        <div className="App">
+                <div className="Header">
+                    <h1>CHESS</h1>
+                </div>
+                <EndTurnBtn onClick={setTurn}/>
+                <PlayerBox playerNumber="1" />
+                <PlayerBox playerNumber="2" />
+                <Board board = {this.entireBoard}/>
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
--- chess-game/src/Components/Board.js
+++ chess-game/src/Components/Board.js
@@ -1,57 +1,83 @@
 import React from 'react';
 import Square from './Square';
+import App from './App';
 
 
-var entireBoard = Array(8).fill(Array(8).fill(<Square/>));
+//var entireBoard = Array(8).fill(Array(8).fill(<Square/>));
 
-const Board = (props) => {
-    //return createBoard();
+class Board extends React.Component{
 
-    return (
-        <div className="board">
-            {generateRow(0)}
-            {generateRow(1)}
-            {generateRow(2)}
-            {generateRow(3)}
-            {generateRow(4)}
-            {generateRow(5)}
-            {generateRow(6)}
-            {generateRow(7)}
-        </div>
-    );
-};
+    //entireBoard = <Square/>[8][8];
+
+    constructor(props){
+        super(props);
+        this.entireBoard = props.entireBoard;
+    }
+
+    render(){
+        return (
+            <div className="board">
+                {this.generateRow(0)}
+                {this.generateRow(1)}
+                {this.generateRow(2)}
+                {this.generateRow(3)}
+                {this.generateRow(4)}
+                {this.generateRow(5)}
+                {this.generateRow(6)}
+                {this.generateRow(7)}
+            </div>
+        );
+    }
+
+    // const Board = (props) => {
+//     //return createBoard();
+//
+//     return (
+//         <div className="board">
+//             {generateRow(props,0)}
+//             {generateRow(props,1)}
+//             {generateRow(props,2)}
+//             {generateRow(props,3)}
+//             {generateRow(props,4)}
+//             {generateRow(5)}
+//             {generateRow(6)}
+//             {generateRow(7)}
+//         </div>
+//     );
+// };
 
 //stitches together the HTML elements that make up the board
-function createBoard(){
-    return(
-        <div className="board">
-            {() => {                                 // Nick if you can look at this...
-                for (let i = 0; i < 8; i++){         // I don't understand why it doesn't work
-                    generateRow(i)                   // it compiles but doesn't show the board
-                }                                    // if you uncomment line 8   -Joe
-            }}
-        </div>
-    );
-}
+    createBoard(){
+        return(
+            <div className="board">
+                {() => {                                 // Nick if you can look at this...
+                    for (let i = 0; i < 8; i++){         // I don't understand why it doesn't work
+                        this.generateRow(i)                   // it compiles but doesn't show the board
+                    }                                    // if you uncomment line 8   -Joe
+                }}
+            </div>
+        );
+    }
 
 
 // Creates a row of board squares. Called once for each of the 8 rows on the board
-function generateRow(currentY){
-    return(
-        <div className="row">
-            {addSquaresToRow(currentY)}
-        </div>
-    );
-}
+    generateRow(currentY){
+        return(
+            <div className="row">
+                {this.addSquaresToRow(currentY)}
+            </div>
+        );
+    }
 
 // HELPER METHOD that adds squares to the primary board data structure
 // then returns the given row so it can be rendered.
-function addSquaresToRow(curY){
-    for (let i = 0; i < 8; i++) {
-        entireBoard[curY][i] = <Square x = {i} y = {curY}/>;
+    addSquaresToRow(curY){
+        for (let i = 0; i < 8; i++) {
+            this.entireBoard[curY][i] = <Square x = {i} y = {curY}/>;
+        }
+        return this.entireBoard[curY];
     }
-    return entireBoard[curY];
 }
 
 export default Board;
-export {entireBoard};
+//export {entireBoard};

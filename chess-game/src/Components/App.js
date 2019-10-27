3dc9af310dedc75550ef05b2966d281424962c1a
Joe Kurtz <jwkurtz@mtu.edu>
2019-10-27 19:28:24 -0400

    Restructured App / Board / Square using classes to see what works

--- chess-game/src/Components/App.js
+++ chess-game/src/Components/App.js
@@ -3,17 +3,33 @@ import '../Style/App.css';
 import PlayerBox from './PlayerBox';
 import Board from './Board';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Header">
-        <h1>CHESS</h1>
-      </div>
-      <PlayerBox playerNumber="1" />
-      <PlayerBox playerNumber="2" />
-      <Board />
-    </div>
-  );
+class App extends React.Component{
+
+    entireBoard = Array(8).fill(Array(8).fill(null));
+    //state;
+
+    constructor(props){
+        super(props);
+        //this.entireBoard = Array(8).fill(Array(8).fill(null));
+        //this.state = null;
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="Header">
+                    <h1>CHESS</h1>
+                </div>
+                <PlayerBox playerNumber="1" />
+                <PlayerBox playerNumber="2" />
+                <Board board = {this.entireBoard}/>
+            </div>
+        );
+    }
+
 }
+
+
 
 export default App;

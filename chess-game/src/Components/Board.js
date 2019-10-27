cef80435ddc991c364c2a1590fe158320c890f9e
Joe <jwkurtz@mtu.edu>
2019-10-27 16:46:01 -0400

    Attempting some structural changes to Board

--- chess-game/src/Components/Board.js
+++ chess-game/src/Components/Board.js
@@ -1,49 +1,72 @@
 import React from 'react';
 import Square from './Square';
 
-let curY = 0;
+const entireBoard = [8][8];
 
-function boardRow(){
-    let rowToReturn = [8];
-
+// uses a loop to add 8 Squares to the main board array
+// method is called once for every square in the row (8 total)
+// returns the given ROW that we're working on
+function addSquaresToRow(curY){
     for (let i = 0; i < 8; i++) {
-        rowToReturn.push(<Square x = {i} y = {curY}/>);
+        entireBoard[curY].push(<Square x = {i} y = {curY}/>);
     }
-    curY++;
-    return rowToReturn;
+    return entireBoard[curY];
 }
 
-const Board = (props) => {
-    let entireBoard = [][8];
-
-    return (
-        <div className="board">
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
-            <div className="row">
-                {boardRow()}
-            </div>
+// HELPER METHOD that creates a row of tile squares
+// method is called once for every row in the board (8 total)
+function generateRow(currentY){
+    return(
+        <div className="row">
+            {addSquaresToRow(currentY)}
         </div>
-    ); 
+    );
+}
+
+// stitches together the HTML elements that make up the board
+function createBoard(){
+    var htmlElements = '<div className="board">';
+    for (let i = 0; i < 8; i++){
+        htmlElements += generateRow(i);
+    }
+    htmlElements += '</div>';
+    return htmlElements;
+}
+
+
+const Board = (props) => {
+    return createBoard();
+
+
+    // return (
+    //     <div className="board">
+    //         <div className="row">
+    //             {addSquaresToRow(0)}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow(1)}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //         <div className="row">
+    //             {addSquaresToRow()}
+    //         </div>
+    //     </div>
+    // );
 };
+
 
 export default Board;

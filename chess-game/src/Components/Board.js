6dcc04586c476104f6a30128b8dc2b336a31e513
Joe Kurtz <jwkurtz@mtu.edu>
2019-10-28 17:54:08 -0400

    Cleaned up code significantly

--- chess-game/src/Components/Board.js
+++ chess-game/src/Components/Board.js
@@ -3,50 +3,47 @@ import Square from './Square';
 
 class Board extends React.Component{
 
+
     // creates the Board, including the 2D-array data structure
     constructor(props) {
         super(props);
-        this.entireBoard = Array(8).fill(Array(8).fill(null));
+
+        this.entireBoard = Array(8).fill(Array(8).fill(null));      // creates an 8x8 array of nulls
+        this.addSquaresToBoard();
     }
 
     // renders the board
     render(){
         return (
             <div className="board">
-                {this.generateAllRows()}
+                {this.entireBoard}
             </div>
         );
     }
 
-    // HELPER METHOD that generates all 8 rows of the board
-    generateAllRows(){
-        let rowArray = [];
-        for ( let i = 0; i < 8; i++){
-            rowArray.push(this.createRow(i));
+    // Fills the board with 8 rows of Squares
+    addSquaresToBoard(){
+        for (let i = 0; i < 8; i++){
+            this.entireBoard.push( this.createRow(i) );
         }
-        return rowArray;
     }
 
-    // HELPER METHOD that creates a row of board squares
-    // this is called once for each of the 8 rows on the board
+    // HELPER METHOD that creates a row of Squares
     createRow(currentY){
         return(
             <div className="row">
-                {this.addSquaresToRow(currentY)}
+                {this.createSquares(currentY)}
             </div>
         );
     }
 
-    // HELPER METHOD that adds squares to the primary board data structure
-    // then returns the given row so it can be rendered.
-    addSquaresToRow(curY){
+    // HELPER METHOD that creates 8 squares and adds them to a row
+    createSquares(currentY){
+        let squareArray = [];
         for (let i = 0; i < 8; i++) {
-
-            // assigns each square its location-aware x / y properties
-            // based on their location inside the primary array
-            this.entireBoard[curY][i] = <Square x = {i} y = {curY}/>;
+            squareArray.push(<Square y = {currentY} x = {i} />);
         }
-        return this.entireBoard[curY];
+        return squareArray;
     }
 }
 

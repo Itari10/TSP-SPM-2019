7c05fb61e11a83f20360463ec110e123f7db54bb
Joe Kurtz <jwkurtz>
2019-10-29 17:28:23 -0400

    Reverts the class structure to Board

--- chess-game/src/Components/Board.js
+++ chess-game/src/Components/Board.js
@@ -1,49 +1,42 @@
 import React from 'react';
 import Square from './Square';
 
-class Board extends React.Component{
+const entireBoard = Array(8).fill(Array(8).fill(null));      // creates an 8x8 array of nulls
 
-    // creates the Board, including the 2D-array data structure
-    constructor(props) {
-        super(props);
-        
-        this.entireBoard = Array(8).fill(Array(8).fill(null));      // creates an 8x8 array of nulls
-        this.addSquaresToBoard();
-    }
-    
-    // renders the board
-    render(){
-        return (
-            <div className="board">
-                {this.entireBoard}
-            </div>
-        );
-    }
+const Board = (props) => {
 
-    // Fills the board with 8 rows of Squares
-    addSquaresToBoard(){
-        for (let i = 0; i < 8; i++){
-            this.entireBoard.push( this.createRow(i) );
-        }
-    }
+    addSquaresToBoard();
 
-    // HELPER METHOD that creates a row of Squares
-    createRow(currentY){
-        return(
-            <div className="row">
-                {this.createSquares(currentY)}
-            </div>
-        );
-    }
+    return (
+        <div className="board">
+            {entireBoard}
+        </div>
+    );
+};
 
-    // HELPER METHOD that creates 8 squares and adds them to a row
-    createSquares(currentY){
-        let squareArray = [];
-        for (let i = 0; i < 8; i++) {
-            squareArray.push(<Square y = {currentY} x = {i} />);
-        }
-        return squareArray;
+// Fills the board with 8 rows of Squares
+function addSquaresToBoard(){
+    for (let i = 0; i < 8; i++){
+        entireBoard.push( createRow(i) );
     }
+}
+
+// HELPER METHOD that creates a row of Squares
+function createRow(currentY){
+    return(
+        <div className="row">
+            {createSquares(currentY)}
+        </div>
+    );
+}
+
+// HELPER METHOD that creates 8 squares and adds them to a row
+function createSquares(currentY){
+    let squareArray = [];
+    for (let i = 0; i < 8; i++) {
+        squareArray.push(<Square y = {currentY} x = {i} />);
+    }
+    return squareArray;
 }
 
 export default Board;

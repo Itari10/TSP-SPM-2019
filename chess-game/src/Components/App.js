1792e328fe0550ae60de974b454c7b6871bb36f7
Nick McCarter <nmccarter10@gmail.com>
2019-11-08 13:57:57 -0500

    Started working on getting some basic boardmovement.

--- chess-game/src/Components/App.js
+++ chess-game/src/Components/App.js
@@ -10,35 +10,26 @@ const App = (props) => {
     const [bState, setBoardState] = React.useState( initializeBoard() );
     const [playerOneTurn, setPlayerOneTurn] = React.useState(true);
     const [updateBoard, setUpdateBoard] = React.useState(true);             // band-aid state
+    const [selectedSquare, setSelectedSquare] = React.useState([-1,-1]);
 
-    const pieceClicked = (y, x) => {
-
-        /*
-         This commented out code is just trying to make it so we don't
-         have to force the re-render using updateboard everytime the
-         boardstate is updated. Currently is broken code
-
-         This is probably just how we're going to have to do it because
-         React is annoying when it comes to arrays. It won't scan the whole
-         thing to see if something has changed.
-        */
-        // setBoardState(boardState.map((mapRow, rowIndex) => {
-        //     mapRow.map((square, colIndex) => {
-        //         if (row === rowIndex && col === colIndex) {
-        //             console.log("foudn match");
-        //             square = "WP";
-        //         }
-        //         return square;
-        //     })
-        //     return mapRow;
-        // }));
-
+    const squareClicked = (y, x) => {
 
         // JANK
-        let temp = bState;
-        temp[y][x].pieceID = "BQ";          // sets the (y, x) that was clicked to queen
-        setBoardState( temp );              // updates the actual board state
-        setUpdateBoard( !updateBoard );       // triggers the app to re-render
+        // let temp = bState;
+        // temp[y][x].pieceID = "BQ";          // sets the (y, x) that was clicked to queen
+        // setBoardState( temp );              // updates the actual board state
+        // setUpdateBoard( !updateBoard );       // triggers the app to re-render
+
+        let boardMap = bState;
+
+        if (selectedSquare[0] === y && selectedSquare[1] === x) {
+            setSelectedSquare([-1,-1]);
+            return;
+        }        
+        setSelectedSquare([y,x]);
+
+
+
     };
 
     //changes the players turn in the game
@@ -63,7 +54,7 @@ const App = (props) => {
                     <PlayerBox playerNumber="2" isTurn={!playerOneTurn}/>
                 </div>
                 <div className="col-sm-8">
-                    <Board bState = {bState} pieceClicked = {pieceClicked}/>
+                    <Board bState = {bState} pieceClicked = {squareClicked}/>
                 </div>
             </div>
         </div>

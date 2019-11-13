ec18ce758aa0fbae391d5cb37cc9469cf7f926ca
Nick McCarter <nmccarter10@gmail.com>
2019-11-13 13:25:01 -0500

    Surrender and end game screen is fully functional but not very pretty.

--- chess-game/src/Components/EndGameScreen.js
+++ chess-game/src/Components/EndGameScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../Style/EndGame.css'
 
 const EndGameScreen = (props) => {
     return (
         <div className="endGameScreen">
             <h1>Guess the game is over now...</h1>
+            <button 
+                className="resetButton"
+                onClick={()=>{window.location.reload()}}
+            >
+                Reset
+            </button>
         </div>
     );
 }

2a7f41517a9059babc34a4063207ee21888a0d75
Justin Martin <justinma@mtu.edu>
2019-12-04 15:24:31 -0500

    Time is currently fully functional now. Was not updating the endGame state before, but now there is a function that will update it and make sure to stop the timer when the game is over. I also added comments to everything in Timer

--- chess-game/src/Components/Timer.js
+++ chess-game/src/Components/Timer.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 
 export default class Timer extends Component {
 
+    //Gather all of the props and put them into Timer State
     constructor(props) {
         super(props);
         this.state = {
@@ -16,17 +17,22 @@ export default class Timer extends Component {
     }
 
 
-
+    //Begins the timer by using an interval
     componentDidMount() {
         this.myInterval = setInterval(() => {
             const { seconds, minutes, isEndGame } = this.state;
+
+            //Check if the game is over by other circumstances, if so, stop timer
             if (isEndGame)
                 this.endTheGame();
+
+            //If there are still seconds left keep going down
             if (seconds > 0) {
                 this.setState(({ seconds }) => ({
                     seconds: seconds - 1
                 }));
             }
+            //if there are no seconds either end the game (if 0 minutes also) or subtract a minute
             if (seconds === 0) {
                 if (minutes === 0) {
                     this.endTheGame();
@@ -37,6 +43,8 @@ export default class Timer extends Component {
         }, 1000)
     }
 
+    //This function will watch for the endGame state to update, if it does we will ensure our local state of endGame
+    //updates and we will then be able to end the game on the next second interval
     componentDidUpdate(prevProps) {
         if (this.props.isEndGame !== prevProps.isEndGame)
             this.setState(({}) => ({
@@ -49,15 +57,16 @@ export default class Timer extends Component {
     }
 
 
-
+    //This will end the game and stop the time
     endTheGame() {
-        const { isTurn, triggerGameOver } = this.state;
+        const { isTurn, triggerGameOver, isEndGame } = this.state;
         clearInterval(this.myInterval);
-        if (isTurn) {
+        if (isTurn && !isEndGame) {
             triggerGameOver();
         }
     }
 
+    //subtracting a minute
     keepGoing() {
         this.setState(({ minutes }) => ({
             minutes: minutes - 1,
